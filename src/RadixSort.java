705258b5d5c7a85802b04898280ee467aea3032e
Willi <Willi@WilliamYaya>
2024-09-17 17:34:49 -0500

    Cambio para que imprima las interaciones

--- src/RadixSort.java
+++ src/RadixSort.java
@@ -5,12 +5,15 @@ public class RadixSort {
     // Método principal de Radix Sort
     public static void radixSort(int[] arr) {
         // Encuentra el número máximo para saber el número de dígitos
-        int max = getMax(arr);
+        int max = getMax(arr); //el numero maximo
 
         // Aplica Counting Sort para cada dígito
-        // En lugar de pasar el número de dígitos, pasamos el exponente (10^i)
+        // En lugar de pasar el número de dígitos, pasamos el exponente (10^i
+        // el exp controla el digito que estamos usando dentro de exp cambia a 1,10, 100
         for (int exp = 1; max / exp > 0; exp *= 10) {
+            System.out.println("Ordenando por digito con exp = " + exp);
             countingSort(arr, exp);
+            System.out.println("Estado del array despues de ordenar por digito con exp = " + exp + ": " + Arrays.toString(arr));
         }
     }
 
@@ -32,6 +35,9 @@ public class RadixSort {
         for (int i = 1; i < 10; i++) {
             count[i] += count[i - 1];
         }
+        /*
+
+        System.out.println("Conteo de digitos para exp = " + exp + ": " + Arrays.toString(count));*/
 
         // Construye la matriz de salida
         for (int i = n - 1; i >= 0; i--) {
@@ -40,7 +46,9 @@ public class RadixSort {
             count[index]--;
         }
 
-        // Copia la matriz de salida en arr[], de modo que arr[] ahora esté ordenado según el numero actual
+        System.out.println("Array de salida despues de ordenar por digito con exp = " + exp + ": " + Arrays.toString(output));
+
+        // Copia la matriz de salida en arr[], de modo que arr[] ahora esté ordenado según el número actual
         for (int i = 0; i < n; i++) {
             arr[i] = output[i];
         }
@@ -59,7 +67,7 @@ public class RadixSort {
 
     public static void main(String[] args) {
         // Declaración e inicialización del arreglo
-        int[] arr = {267, 33, 85, 99, 952, 14, 1, 80};
+        int[] arr = {267, 33, 85, 99, 952, 14, 1, 80,532};
 
         // Mostrar el arreglo antes de ordenar
         System.out.println("Arreglo antes del ordenamiento: " + Arrays.toString(arr));
